7325f56e321c3ad252014cd901d8a2d6fa9703bd
reushftw <fathallah.ghassen97@gmail.com>
2019-02-26 15:52:46 +0100

    reush pushing

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use BlogBundle\Entity\BlogPost;
 use ContactBundle\Entity\Message;
 use ContactBundle\Form\MessageType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -28,6 +29,128 @@ class DefaultController extends Controller
         }
 
         return $this->render('default/index.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * @Route("/news", name="newspage")
+     */
+    public function newsAction(Request $request)
+    {
+        $blog=$this->getDoctrine()->getManager()->getRepository(BlogPost::class)->findBy(array('enabled'=>true));
+
+        return $this->render('default/news.html.twig', array(
+            'blog' => $blog
+        ));
+    }
+
+    /**
+     * @Route("/assistance", name="assistancepage")
+     */
+    public function assistanceAction(Request $request)
+    {
+        $message = new Message();
+        $message->setEnabled(true);
+        $form = $this->get('form.factory')->create(MessageType::class, $message);
+
+        if($request->isMethod('POST') && $form->handleRequest($request)->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($message);
+            $em->flush();
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('default/assistance.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * @Route("/audit", name="auditpage")
+     */
+    public function auditAction(Request $request)
+    {
+        $message = new Message();
+        $message->setEnabled(true);
+        $form = $this->get('form.factory')->create(MessageType::class, $message);
+
+        if($request->isMethod('POST') && $form->handleRequest($request)->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($message);
+            $em->flush();
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('default/audit.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * @Route("/training", name="trainingpage")
+     */
+    public function trainingAction(Request $request)
+    {
+        $message = new Message();
+        $message->setEnabled(true);
+        $form = $this->get('form.factory')->create(MessageType::class, $message);
+
+        if($request->isMethod('POST') && $form->handleRequest($request)->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($message);
+            $em->flush();
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('default/training.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * @Route("/laboratory", name="laboratorypage")
+     */
+    public function laboratoryAction(Request $request)
+    {
+        $message = new Message();
+        $message->setEnabled(true);
+        $form = $this->get('form.factory')->create(MessageType::class, $message);
+
+        if($request->isMethod('POST') && $form->handleRequest($request)->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($message);
+            $em->flush();
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('default/laboratory.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * @Route("/aboutus", name="aboutuspage")
+     */
+    public function aboutusAction(Request $request)
+    {
+        $message = new Message();
+        $message->setEnabled(true);
+        $form = $this->get('form.factory')->create(MessageType::class, $message);
+
+        if($request->isMethod('POST') && $form->handleRequest($request)->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($message);
+            $em->flush();
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('default/aboutus.html.twig', array(
             'form' => $form->createView(),
         ));
     }
